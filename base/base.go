06b8b016089e5a2926a612e190b498fe5efb3c92
Toby Burress <kurin@google.com>
2016-04-28 13:13:33 -0400

    Lots of everything.

--- base/base.go
+++ base/base.go
@@ -1,4 +1,20 @@
 // Package base provides a very low-level interface on top of the B2 v1 API.
+// It is not intended to be used directly.
+//
+// It currently lacks support for the following APIs:
+//
+// b2_create_bucket
+// b2_delete_bucket
+// b2_delete_file_version
+// b2_download_file_by_id
+// b2_download_file_by_name
+// b2_get_file_info
+// b2_hide_file
+// b2_list_file_names
+// b2_list_file_versions
+// b2_list_parts
+// b2_list_unfinished_large_files
+// b2_update_bucket
 package base
 
 import (
@@ -7,7 +23,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
 const (
@@ -15,6 +34,7 @@ const (
 	apiV1   = "/b2api/v1/"
 )
 
+// B2 holds account information for Backblaze.
 type B2 struct {
 	accountID   string
 	authToken   string
@@ -23,49 +43,79 @@ type B2 struct {
 	minPartSize int
 }
 
-var Err = errors.New("b2 api error")
-
 type b2AuthorizeAccountResponse struct {
 	AccountID   string `json:"accountId"`
 	AuthToken   string `json:"authorizationToken"`
-	ApiURI      string `json:"apiUrl"`
+	URI         string `json:"apiUrl"`
 	DownloadURI string `json:"downloadUrl"`
 	MinPartSize int    `json:"minimumPartSize"`
+}
+
+type errMsg struct {
+	Msg string `json:"message"`
+}
+
+func makeRequest(verb, url string, b2req, b2resp interface{}, headers map[string]string, body io.Reader) error {
+	if b2req != nil {
+		enc, err := json.Marshal(b2req)
+		if err != nil {
+			return err
+		}
+		body = bytes.NewBuffer(enc)
+	}
+	req, err := http.NewRequest(verb, url, body)
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		msg := &errMsg{}
+		if err := json.Unmarshal(data, msg); err != nil {
+			return err
+		}
+		return errors.New(msg.Msg)
+	}
+	if b2resp != nil {
+		decoder := json.NewDecoder(resp.Body)
+		if err := decoder.Decode(b2resp); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // B2AuthorizeAccount wraps b2_authorize_account.
 func B2AuthorizeAccount(account, key string) (*B2, error) {
 	auth := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", account, key)))
-	req, err := http.NewRequest("GET", apiBase+apiV1+"b2_authorize_account", nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("Basic %s", auth))
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return nil, Err // TODO: actually handle errors
-	}
-	decoder := json.NewDecoder(resp.Body)
 	b2resp := &b2AuthorizeAccountResponse{}
-	if err := decoder.Decode(b2resp); err != nil {
+	headers := map[string]string{
+		"Authorization": fmt.Sprintf("Basic %s", auth),
+	}
+	if err := makeRequest("GET", apiBase+apiV1+"b2_authorize_account", nil, b2resp, headers, nil); err != nil {
 		return nil, err
 	}
 	return &B2{
 		accountID:   b2resp.AccountID,
 		authToken:   b2resp.AuthToken,
-		apiURI:      b2resp.ApiURI,
+		apiURI:      b2resp.URI,
 		downloadURI: b2resp.DownloadURI,
 		minPartSize: b2resp.MinPartSize,
 	}, nil
 }
 
+// Bucket holds B2 bucket details.
 type Bucket struct {
 	Name string
 	id   string
+	b2   *B2
 }
 
 type b2ListBucketsRequest struct {
@@ -80,38 +130,210 @@ type b2ListBucketsResponse struct {
 	} `json:"buckets"`
 }
 
+// ListBuckets wraps b2_list_buckets.
 func (b *B2) ListBuckets() ([]Bucket, error) {
 	b2req := &b2ListBucketsRequest{
 		AccountID: b.accountID,
 	}
-	enc, err := json.Marshal(b2req)
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequest("POST", b.apiURI+apiV1+"b2_list_buckets", bytes.NewBuffer(enc))
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Authorization", b.authToken)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("err %d", resp.StatusCode)
-	}
 	b2resp := &b2ListBucketsResponse{}
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(b2resp); err != nil {
+	headers := map[string]string{
+		"Authorization": b.authToken,
+	}
+	if err := makeRequest("POST", b.apiURI+apiV1+"b2_list_buckets", b2req, b2resp, headers, nil); err != nil {
 		return nil, err
 	}
 	var buckets []Bucket
-	for _, b := range b2resp.Buckets {
+	for _, bucket := range b2resp.Buckets {
 		buckets = append(buckets, Bucket{
-			Name: b.BucketName,
-			id:   b.BucketID,
+			Name: bucket.BucketName,
+			id:   bucket.BucketID,
+			b2:   b,
 		})
 	}
 	return buckets, nil
+}
+
+type b2GetUploadURLRequest struct {
+	BucketID string `json:"bucketId"`
+}
+
+type b2GetUploadURLResponse struct {
+	URI   string `json:"uploadUrl"`
+	Token string `json:"authorizationToken"`
+}
+
+// UploadEndpoint holds information from the b2_get_upload_url API.
+type UploadEndpoint struct {
+	uri   string
+	token string
+}
+
+// GetUploadURL wraps b2_get_upload_url.
+func (b Bucket) GetUploadURL() (*UploadEndpoint, error) {
+	b2req := &b2GetUploadURLRequest{
+		BucketID: b.id,
+	}
+	b2resp := &b2GetUploadURLResponse{}
+	headers := map[string]string{
+		"Authorization": b.b2.authToken,
+	}
+	if err := makeRequest("POST", b.b2.apiURI+apiV1+"b2_get_upload_url", b2req, b2resp, headers, nil); err != nil {
+		return nil, err
+	}
+	return &UploadEndpoint{
+		uri:   b2resp.URI,
+		token: b2resp.Token,
+	}, nil
+}
+
+// UploadFile wraps b2_upload_file.
+func (ue UploadEndpoint) UploadFile(r io.Reader, size int, name, contentType, sha1 string, info map[string]string) error {
+	headers := map[string]string{
+		"Authorization":     ue.token,
+		"X-Bz-File-Name":    name,
+		"Content-Type":      contentType,
+		"Content-Length":    fmt.Sprintf("%d", size),
+		"X-Bz-Content-Sha1": sha1,
+	}
+	for k, v := range info {
+		headers[fmt.Sprintf("X-Bz-Info-%s", k)] = v
+	}
+	return makeRequest("POST", ue.uri, nil, nil, headers, r)
+}
+
+type startLargeFileRequest struct {
+	BucketID    string            `json:"bucketId"`
+	Name        string            `json:"fileName"`
+	ContentType string            `json:"contentType"`
+	Info        map[string]string `json:"fileInfo"`
+}
+
+type startLargeFileResponse struct {
+	ID string `json:"fileId"`
+}
+
+// LargeFile holds information necessary to implement B2 large file support.
+type LargeFile struct {
+	id string
+	b2 *B2
+
+	mu     sync.Mutex
+	hashes map[int]string
+}
+
+// StartLargeFile wraps b2_start_large_file.
+func (b Bucket) StartLargeFile(name, contentType string, info map[string]string) (*LargeFile, error) {
+	b2req := &startLargeFileRequest{
+		BucketID:    b.id,
+		Name:        name,
+		ContentType: contentType,
+		Info:        info,
+	}
+	b2resp := &startLargeFileResponse{}
+	headers := map[string]string{
+		"Authorization": b.b2.authToken,
+	}
+	if err := makeRequest("POST", b.b2.apiURI+apiV1+"b2_start_large_file", b2req, b2resp, headers, nil); err != nil {
+		return nil, err
+	}
+	return &LargeFile{
+		id:     b2resp.ID,
+		b2:     b.b2,
+		hashes: make(map[int]string),
+	}, nil
+}
+
+type cancelLargeFileRequest struct {
+	ID string `json:"fileId"`
+}
+
+// CancelLargeFile wraps b2_cancel_large_file.
+func (l LargeFile) CancelLargeFile() error {
+	b2req := &cancelLargeFileRequest{
+		ID: l.id,
+	}
+	headers := map[string]string{
+		"Authorization": l.b2.authToken,
+	}
+	if err := makeRequest("POST", l.b2.apiURI+apiV1+"b2_cancel_large_file", b2req, nil, headers, nil); err != nil {
+		return err
+	}
+	return nil
+}
+
+// FileChunk holds information necessary for uploading file chunks.
+type FileChunk struct {
+	url   string
+	token string
+	file  *LargeFile
+}
+
+type getUploadPartURLRequest struct {
+	ID string `json:"fileId"`
+}
+
+type getUploadPartURLResponse struct {
+	URL   string `json:"uploadUrl"`
+	Token string `json:"authorizationToken"`
+}
+
+// GetUploadPartURL wraps b2_get_upload_part_url.
+func (l *LargeFile) GetUploadPartURL() (*FileChunk, error) {
+	b2req := &getUploadPartURLRequest{
+		ID: l.id,
+	}
+	b2resp := &getUploadPartURLResponse{}
+	headers := map[string]string{
+		"Authorization": l.b2.authToken,
+	}
+	if err := makeRequest("POST", l.b2.apiURI+apiV1+"b2_get_upload_part_url", b2req, b2resp, headers, nil); err != nil {
+		return nil, err
+	}
+	return &FileChunk{
+		url:   b2resp.URL,
+		token: b2resp.Token,
+		file:  l,
+	}, nil
+}
+
+// UploadPart wraps b2_upload_part.
+func (fc *FileChunk) UploadPart(r io.Reader, sha1 string, size, index int) (int, error) {
+	headers := map[string]string{
+		"Authorization":     fc.token,
+		"X-Bz-Part-Number":  fmt.Sprintf("%d", index),
+		"Content-Length":    fmt.Sprintf("%d", size),
+		"X-Bz-Content-Sha1": sha1,
+	}
+	if err := makeRequest("POST", fc.url, nil, nil, headers, r); err != nil {
+		return 0, err
+	}
+	fc.file.mu.Lock()
+	fc.file.hashes[index] = sha1
+	fc.file.mu.Unlock()
+	return int(size), nil
+}
+
+type finishLargeFileRequest struct {
+	ID     string   `json:"fileId"`
+	Hashes []string `json:"partSha1Array"`
+}
+
+// FinishLargeFile wraps b2_finish_large_file.
+func (l *LargeFile) FinishLargeFile() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	b2req := &finishLargeFileRequest{
+		ID:     l.id,
+		Hashes: make([]string, len(l.hashes)),
+	}
+	for k, v := range l.hashes {
+		b2req.Hashes[k-1] = v
+	}
+	headers := map[string]string{
+		"Authorization": l.b2.authToken,
+	}
+	if err := makeRequest("POST", l.b2.apiURI+apiV1+"b2_finish_large_file", b2req, nil, headers, nil); err != nil {
+		return err
+	}
+	return nil
 }
---
+++ gozer.go
@@ -0,0 +1,202 @@
+package gozer
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"hash"
+	"io"
+	"log"
+	"sync"
+
+	"github.com/kurin/gozer/base"
+)
+
+// B2 is a Backblaze connection.
+type B2 struct {
+	b2 *base.B2
+}
+
+// New returns a new connection to Backblaze B2.
+func New(account, key string) (*B2, error) {
+	b2, err := base.B2AuthorizeAccount(account, key)
+	if err != nil {
+		return nil, err
+	}
+	return &B2{
+		b2: b2,
+	}, nil
+}
+
+// Bucket is a reference to a B2 bucket.
+type Bucket struct {
+	b base.Bucket
+}
+
+// Bucket returns the named bucket, if it exists.
+func (b *B2) Bucket(name string) (*Bucket, error) {
+	buckets, err := b.b2.ListBuckets()
+	if err != nil {
+		return nil, err
+	}
+	for _, bucket := range buckets {
+		if bucket.Name == name {
+			return &Bucket{
+				b: bucket,
+			}, nil
+		}
+	}
+	// TODO: create bucket
+	return nil, fmt.Errorf("%s: no such bucket", name)
+}
+
+// NewWriter returns a new writer for the given file.
+func (b *Bucket) NewWriter(name, contentType string, info map[string]string) *B2Writer {
+	bw := &B2Writer{
+		bucket: b.b,
+		name:   name,
+		ctype:  contentType,
+		info:   info,
+		chsh:   sha1.New(),
+		cbuf:   &bytes.Buffer{},
+	}
+	bw.w = io.MultiWriter(bw.chsh, bw.cbuf)
+	return bw
+}
+
+type chunk struct {
+	id      int
+	attempt int
+	size    int
+	sha1    string
+	buf     *bytes.Buffer
+}
+
+// B2Writer writes data into Backblaze.  It automatically switches to the large
+// file API if the file exceeds 100MB (that is, 1e8 bytes).  Due to that and
+// other Backblaze API details, there is a large (100MB) buffer.
+type B2Writer struct {
+	// ConcurrentUploads is number of different threads sending data concurrently
+	// to Backblaze for large files.  This can increase performance greatly, as
+	// each thread will hit a different endpoint.  However, there is a 100MB
+	// buffer for each thread.  Values less than 1 are equivalent to 1.
+	ConcurrentUploads int
+
+	// TotalRetries is the number of times a failed partial write will be retried
+	// until the operation returns an error.
+	TotalRetries int
+
+	ready chan chunk
+	wg    sync.WaitGroup
+	once  sync.Once
+	file  *base.LargeFile
+
+	bucket base.Bucket
+	name   string
+	ctype  string
+	info   map[string]string
+
+	cbuf *bytes.Buffer
+	cidx int
+	chsh hash.Hash
+	w    io.Writer
+}
+
+func (bw *B2Writer) thread() {
+	go func() {
+		fc, err := bw.file.GetUploadPartURL()
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		bw.wg.Add(1)
+		defer bw.wg.Done()
+		for {
+			chunk, ok := <-bw.ready
+			if !ok {
+				return
+			}
+			if _, err := fc.UploadPart(chunk.buf, chunk.sha1, chunk.size, chunk.id); err != nil {
+				log.Print(err)
+				chunk.attempt++
+				bw.ready <- chunk
+				continue
+			}
+		}
+	}()
+}
+
+// Write satisfies the io.Writer interface.
+func (bw *B2Writer) Write(p []byte) (int, error) {
+	left := 1e8 - bw.cbuf.Len()
+	if len(p) < left {
+		return bw.w.Write(p)
+	}
+	i, err := bw.w.Write(p[:left])
+	if err != nil {
+		return i, err
+	}
+	if err := bw.sendChunk(); err != nil {
+		return i, err
+	}
+	k, err := bw.Write(p[left:])
+	return i + k, err
+}
+
+func (bw *B2Writer) simpleWriteFile() error {
+	ue, err := bw.bucket.GetUploadURL()
+	if err != nil {
+		return err
+	}
+	sha1 := fmt.Sprintf("%x", bw.chsh.Sum(nil))
+	return ue.UploadFile(bw.cbuf, bw.cbuf.Len(), bw.name, bw.ctype, sha1, bw.info)
+}
+
+func (bw *B2Writer) sendChunk() error {
+	var err error
+	bw.once.Do(func() {
+		lf, e := bw.bucket.StartLargeFile(bw.name, bw.ctype, bw.info)
+		if e != nil {
+			err = e
+			return
+		}
+		bw.file = lf
+		bw.ready = make(chan chunk)
+		if bw.ConcurrentUploads < 1 {
+			bw.ConcurrentUploads = 1
+		}
+		for i := 0; i < bw.ConcurrentUploads; i++ {
+			bw.thread()
+		}
+	})
+	if err != nil {
+		return err
+	}
+	bw.ready <- chunk{
+		id:   bw.cidx + 1,
+		size: bw.cbuf.Len(),
+		sha1: fmt.Sprintf("%x", bw.chsh.Sum(nil)),
+		buf:  bw.cbuf,
+	}
+	bw.cidx++
+	bw.chsh = sha1.New()
+	bw.cbuf = &bytes.Buffer{}
+	bw.w = io.MultiWriter(bw.chsh, bw.cbuf)
+	return nil
+}
+
+// Close satisfies the io.Closer interface.  It should not be called more than
+// once.
+func (bw *B2Writer) Close() error {
+	if bw.cidx == 0 {
+		return bw.simpleWriteFile()
+	}
+	if bw.cbuf.Len() > 0 {
+		if err := bw.sendChunk(); err != nil {
+			return err
+		}
+	}
+	close(bw.ready)
+	bw.wg.Wait()
+	return bw.file.FinishLargeFile()
+}

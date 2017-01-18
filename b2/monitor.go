f031403e7e40f4c9caabb2f5c7d9b37fa5b708eb
Toby Burress <kurin@google.com>
2017-01-18 08:25:18 -0800

    b2: add an http server to monitor state

---
+++ b2/monitor.go
@@ -0,0 +1,29 @@
+// Copyright 2017, Google
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package b2
+
+import "net/http"
+
+// ShowStats causes b2 to listen for http on the given network address, where
+// it displays information about what it's doing.
+func (c *Client) ShowStats(addr string) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", c.infoHandler)
+	go http.ListenAndServe(addr, mux)
+}
+
+func (c *Client) infoHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Write([]byte("hello, world"))
+}

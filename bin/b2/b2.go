648c507d7d4aedf93793b3c1b48146d6eab11beb
Toby Burress <kurin@google.com>
2017-10-17 05:11:52 +0000

    b2: add a `b2` command, more or less because why not.

---
+++ bin/b2/b2.go
@@ -0,0 +1,18 @@
+// b2 is a command-line tool for interacting with the Backblaze B2 service.  It
+// mimics https://github.com/Backblaze/B2_Command_Line_Tool as much as it is
+// able.
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+
+	"github.com/google/subcommands"
+)
+
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+	os.Exit(int(subcommands.Execute(ctx)))
+}
